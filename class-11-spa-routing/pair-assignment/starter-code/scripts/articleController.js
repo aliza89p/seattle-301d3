0fb0ac4e05282b7b235fab98fa0b903b3f236ab7
Aliza Pilisuk <apilisuk@gmail.com>
2016-04-25 13:00:49 -0700

    finished stretch goal

--- class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -9,7 +9,7 @@
     // DONE: Complete this function that kicks off the fetching and rendering
     //  of articles, using the same
     //  code that used to be in index.html:
-  
+
     Article.fetchAll(articleView.initIndexPage);
     // DONE: But wait! There's more: Also be sure to hide all the main section
     //  elements, and reveal the articles section:
