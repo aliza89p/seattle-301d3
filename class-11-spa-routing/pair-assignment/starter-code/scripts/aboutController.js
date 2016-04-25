c78874ab562051becc69c6d078edb1fe957e36f5
Dustin McBride <dustinbm@gmail.com>
2016-04-25 12:29:24 -0700

    hide articles, show about

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -5,6 +5,7 @@
     // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
 
     //don't need to create a table since not doing anything. Just the show about page.
+    $('#articles').hide();
     $('#about').show();
 
   };
