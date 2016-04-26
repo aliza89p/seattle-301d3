c41f5c39961c035fca3da7c6d3c52e3158adf642
Caleb Sattgast <calebsattgast@gmail.com>
2016-04-26 14:16:23 -0700

    set up AJAX call to pull in github data

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/model/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/model/repo.js
@@ -6,7 +6,17 @@
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Someone say AJAX!?
     //  Don't forget to call the callback!
-
+    $.ajax({
+      url: 'https://api.github.com/users/maschigokae/repos' +
+      '?per_page=10' +
+      '&sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data) {
+        repos.all = data;
+        callback();
+      }
+    });
   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
